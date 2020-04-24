f10e3220a874ac03461e8db1a46cbf5a093c4c86
sketchbook-mac <sos891214@gmail.com>
2020-04-24 19:01:39 +0900

    Indexer 생성

--- src/main/java/sketchbook/kr/lucene/Indexer.java
+++ src/main/java/sketchbook/kr/lucene/Indexer.java
@@ -2,11 +2,15 @@ package sketchbook.kr.lucene;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -19,10 +23,15 @@ public class Indexer {
 	
 	public Indexer(String indexDir) throws IOException {
 		
+		//create or open directory 
 		Directory dir = FSDirectory.open(new File(indexDir));
+		
+		//Analyzer 는 Text를 잘게 자라서 Term을 만드는 작업을 수행한다.
 		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_46);
 		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_46, analyzer);
 		
+		//첫번째 디렉토리 lucene index 를 저장할 디렉토리
+		//두번째 디렉토리는  대상 파일이 들어있는 디렉토리이다.
 		writer = new IndexWriter(dir, config);
 	}
 	
@@ -37,6 +46,7 @@ public class Indexer {
 		
 		long start = System.currentTimeMillis();
 		
+		//lucene index directory
 		Indexer indexer = new Indexer(indexDir);
 		
 		int numIndexed;
@@ -55,21 +65,41 @@ public class Indexer {
 	}
 	
 	public void close() throws IOException {
+		writer.close();
+	}
+	
+	public int index(String dataDir, FileFilter filter) throws Exception {
+		
+		File[] files = new File(dataDir).listFiles();
+		
+		for(File file :files) {
+			System.out.println(file.getName());
+			if(!file.isDirectory() && !file.isHidden() && !file.exists() && file.canRead() && (filter == null || filter.accept(file))) {
+				indexFile(file);
+			}
+		}
+		return writer.numDocs();
+	}
+	
+	//Add Document 
+	private void indexFile(File file) throws Exception{
+		//getCanonicalPath : 현재 프로그램을 실행한 경로를 포함한 파일경로를 반환한다.
+		System.out.println("indexing : "+ file.getCanonicalPath());
+		Document document = getDocument(file);
+		writer.addDocument(document);
 		
 	}
 	
-	public int index(String dataDir, FileFilter filter) {
+	//Get Document
+	protected Document getDocument(File file) throws Exception {
+		Document doc = new Document();
+		doc.add(new TextField("contents",new FileReader(file)));
 		
-		return 0;
-	}
-	
-	private void indexFile(File f) {
+		doc.add(new TextField("filename", file.getName(),Field.Store.YES));
 		
-	}
-	
-	protected Document getDocument(File f) {
+		doc.add(new StringField("fullpath",file.getCanonicalPath(),Field.Store.YES));
 		
-		return null;
+		return doc;
 	}
 	
 	//색인할 대상 txt 파일 목록을 뽑아낸다.
---
+++ src/main/java/sketchbook/kr/lucene/Search.java
@@ -0,0 +1,10 @@
+package sketchbook.kr.lucene;
+
+public class Search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
